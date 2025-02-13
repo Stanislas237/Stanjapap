ce25b72fc05dd1f4fb51420a6175cb6c21a33bfe
Stanislas KAMGA <stankamga00@gmail.com>
2025-02-14 00:01:39 +0100

    update

--- functions/index.js
+++ functions/index.js
@@ -36,6 +36,25 @@ async function checkAuth(req, email) {
         return false;
     }
 }
+function deepEqual(obj1, obj2) {
+    if (typeof obj1 !== "object" || typeof obj2 !== "object" || obj1 === null || obj2 === null)
+        return obj1 === obj2;
+
+    const normalizeMessage = (msg) => {
+        return JSON.stringify({
+            senderMail: msg.senderMail,
+            receiverMail: msg.receiverMail,
+            content: msg.content,
+            read: msg.read ?? false,
+            tag: msg.tag ?? "",
+            timestamp: {
+                seconds: msg.timestamp.seconds ?? msg.timestamp._seconds,
+            }
+        });
+    }
+    
+    return normalizeMessage(obj1) === normalizeMessage(obj2);
+}
 
 exports.addUser = onRequest(async (req, res) => {
     res.set("Access-Control-Allow-Origin", "https://stanjapap.web.app");
@@ -157,7 +176,7 @@ exports.getFriends = onRequest(async (req, res) => {
 
         const alreadyExists = (elt, list) =>{
             for (var e of list)
-                if (JSON.stringify(elt) === JSON.stringify(e))
+                if (deepEqual(e, elt))
                     return true;
             return false;
         }
@@ -189,5 +208,42 @@ exports.getFriends = onRequest(async (req, res) => {
     } catch (error) {
         console.error("Error fetching messages:", error);
         return res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+exports.sendMessage = onRequest(async (req, res) => {
+    res.set("Access-Control-Allow-Origin", "https://stanjapap.web.app");
+    res.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS")
+        return res.status(204).send("");
+    else if (req.method !== "POST")
+        return res.status(405).json({ "error": "Méthode non autorisée" });
+    
+    const { senderMail, receiverMail, content, tag } = req.body;
+    if (!senderMail || !receiverMail || !content)
+        return res.status(400).json({ error: "Bad Request" });
+
+    if (!await checkAuth(req, senderMail))
+        return res.status(401).json({ error: "Unauthenticated" });
+
+    const timestamp = admin.firestore.Timestamp.now();
+
+    const message = {
+        "senderMail": senderMail,
+        "receiverMail": receiverMail,
+        "content": content,
+        "tag": tag ?? "",
+        "read": false,  // Par défaut, le message n'est pas lu
+        "timestamp": timestamp
+    };
+
+    try {
+        await db.collection("messages").add(message);
+        return res.status(200).json({ "message": message });
+    } catch (error) {
+        console.error("Erreur lors de l'envoi du message:", error);
+        return res.status(500).send("Internal error");
     }
 });
