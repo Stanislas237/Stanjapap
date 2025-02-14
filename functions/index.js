07265ba6e624edb0c07d8bdf10c930e6c76c336f
Stanislas KAMGA <stankamga00@gmail.com>
2025-02-14 14:18:36 +0100

    update

--- functions/index.js
+++ functions/index.js
@@ -75,8 +75,9 @@ exports.addUser = onRequest(async (req, res) => {
             return res.status(409).json({ "error": "User already exists" });
 
         userRef.set({
-            name,
-            pseudo,
+            name: name,
+            pseudo: pseudo,
+            actu: "Disponible",
             aliases: {},
             ppUrl: "https://as2.ftcdn.net/v2/jpg/01/99/45/45/1000_F_199454533_GIBKQvbUBlu0hl5xhn64pJOHp1nn5W2C.jpg",
             registerDate: admin.firestore.FieldValue.serverTimestamp()
@@ -159,10 +160,10 @@ exports.getFriends = onRequest(async (req, res) => {
             }
             if (!alreadyExists(msg, conversations[otherUser].messages)){
                 conversations[otherUser].messages.push(msg);
-                conversations[otherUser].lastMessage = { "seconds": msg.timestamp.seconds ?? msg.timestamp._seconds, "content": msg.content };    
-                if (isUnread) {
+                if (!conversations[otherUser].lastMessage)
+                    conversations[otherUser].lastMessage = { "seconds": msg.timestamp.seconds ?? msg.timestamp._seconds, "content": msg.content };    
+                if (isUnread)
                     conversations[otherUser].unreadCount++;
-                }
             }
         };
 
@@ -213,6 +214,75 @@ exports.sendMessage = onRequest(async (req, res) => {
         return res.status(200).json({ "message": message });
     } catch (error) {
         console.error("Erreur lors de l'envoi du message:", error);
+        return res.status(500).send("Internal error");
+    }
+});
+
+exports.saveAlias = onRequest(async (req, res) => {
+    res.set("Access-Control-Allow-Origin", "https://stanjapap.web.app");
+    res.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS")
+        return res.status(204).send("");
+    else if (req.method !== "POST")
+        return res.status(405).json({ "error": "Méthode non autorisée" });
+    
+    const { email, contact, alias } = req.body;
+    if (!email || !contact || !alias)
+        return res.status(400).json({ error: "Bad Request" });
+
+    if (!await checkAuth(req, email))
+        return res.status(401).json({ error: "Unauthenticated" });
+
+    try {
+        const userRef = db.collection("users").doc(email);
+        const user = await userRef.get();
+        if (!user.exists)
+            return res.status(404).json({ "error": "User doesn't exist" });
+
+        const aliases = user.data().aliases ?? {};
+        aliases[contact] = alias;
+
+        userRef.update({ "aliases": aliases });
+        return res.status(200).json({ "aliases": aliases });
+    } catch (error) {
+        console.error("Erreur lors de l'enregistrement du contact : ", error);
+        return res.status(500).send("Internal error");
+    }
+});
+
+exports.getProfile = onRequest(async (req, res) => {
+    res.set("Access-Control-Allow-Origin", "https://stanjapap.web.app");
+    res.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS")
+        return res.status(204).send("");
+    else if (req.method !== "POST")
+        return res.status(405).json({ "error": "Méthode non autorisée" });
+    
+    const { email } = req.body;
+    if (!email)
+        return res.status(400).json({ error: "Bad Request" });
+
+    try {
+        const userRef = db.collection("users").doc(email);
+        const user = await userRef.get();
+        if (!user.exists)
+            return res.status(404).json({ "error": "User doesn't exist" });
+
+        const profile = {
+            "ppUrl": user.data().ppUrl,
+            "pseudo": user.data().pseudo,
+            "actu": user.data().actu
+        };
+        if (user.data().showEmail)
+            profile["email"] = email;
+        
+        return res.status(200).json({ "profile": profile });
+    } catch (error) {
+        console.error("Erreur de getProfile : ", error);
         return res.status(500).send("Internal error");
     }
 });
